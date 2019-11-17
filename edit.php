8303734ff2f22a2077e0d21a7d9a2c30f51eadc6
Charlie Brinicombe <55358291+cbrinicombe13@users.noreply.github.com>
2019-11-17 10:45:00 +0000

    Add files via upload
    Finished delete.php and edit.php whilst separating search.php from edit.php.

--- edit.php
+++ edit.php
@@ -1,69 +1,69 @@
-<?php
-    include("entry.php");
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Address Book: Search/Edit Entry</title>
+    <title>Address Book: Edit</title>
 </head>
 <body>
-    <h1>Search/Edit Entry </h1><hr><br>
-<!-- Search Form -->
-    <div>
-        Search below first: <br><br>
-        <form action="" method = "post">
-            <input type = "text" name = "first" placeholder = "First Name"><br>
-            <input type = "text" name = "last" placeholder = "Last Name"><br>
-            <br><input type = "submit"><br>
-        </form><br>
-    </div>
-<!-- Get search parameters -->
-    <?php
-    if(isset($_POST["first"], $_POST["last"])) {
-        $first = $_POST["first"];
-        $last = $_POST["last"];
-    }
+    <h1>Edit</h1><hr><br>
 
+<!--  Get '$item' and '$key' from search.php and make '$json_arr' -->
+
+    <?php
     $json = file_get_contents("JSON.txt");
     $json_arr = json_decode($json, true);
-    ?>
-<!-- Check for matches in JSON -->
-    <?php foreach($json_arr as $item) : ?>
-        <? if ($item["first_name"] == $first && $item["last_name"] == $last) : ?>
-        <!-- If match show details -->
-            <hr><br>Found this:<br><br>
-            <table>
-                <tbody>
-                    <tr>
-                        First Name: <?php echo $item["first_name"]; ?><br>
-                        Last Name: <?php echo $item["last_name"]; ?><br>
-                        Phone: <?php echo $item["phone"]; ?><br>
-                        E-Mail: <?php echo $item["email"]; ?><br>
-                    </tr>
-                </tbody>
-            </table><hr><br>
-            Make changes below: <br><br>
-            <form action="">
-                <input type="text" name = "new_first" placeholder = "New First Name"><br>
-                <input type="text" name = "new_last" placeholder = "New Last Name"><br>
-                <input type="text" name = "new_phone" placeholder = "New Phone"><br>
-                <input type="email" name = "new_email" placeholder = "New E-Mail"><br><br>
-                <input type="submit" value = "Submit Changes"><br><br>
-            </form>
-            <?php  
-                if(isset($_POST["new_first"])) {
-                    $new_first = $_POST["new_first"];
-                    echo $new_first;
-                }
-            ?>
 
-        <?php endif; ?>
-    <?php endforeach; ?>
-<!-- Go Home -->
+    parse_str($_GET["current_item"]);
+    // Can now access 'item' as 'current_item'.
+
+    parse_str($_GET["current_key"]);
+    // Can now access 'key' as 'current_key'.
+
+    ?>
+
+<!-- Form for the changes -->
+    <form action="" method = "post">
+        <input type="text" name = "new_first" placeholder = "New First Name"><br>
+        <input type="text" name = "new_last" placeholder = "New Last Name"><br>
+        <input type="text" name = "new_phone" placeholder = "New Phone"><br>
+        <input type="email" name = "new_email" placeholder = "New E-Mail"><br>
+        <input type="submit" name = "new_submit" value = "Submit Changes"><br>
+    </form>
+
+<!--  Make changes -->
+    <?php
+    $new_first = $_POST["new_first"];
+    $new_last = $_POST["new_last"];
+    $new_phone = $_POST["new_phone"];
+    $new_email = $_POST["new_email"];
+
+    if(isset($_POST["new_submit"])) {
+        if(!empty($new_first)) {
+            $current_item["first_name"] = $new_first; 
+        }
+        if(!empty($new_last)) {
+            $current_item["last_name"] = $new_last; 
+        }
+        if(!empty($new_phone)) {
+            $current_item["phone"] = $new_phone; 
+        }
+        if(!empty($new_email)) {
+            $current_item["email"] = $new_email;
+        }
+
+        // Replace with changes:
+
+        $json_arr[$current_key] = $current_item;
+
+        file_put_contents("JSON.txt",json_encode($json_arr));
+        
+    }
+
+    ?>
+
+    <!-- Go Home -->
     <hr>
     <form action = "/addressbook/index.html"><br>
         <input type = "submit" value = "Home">
--- veiw.php
+++ veiw.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Address Book: View Book</title>
+    <title>Address Book: View</title>
 </head>
 <body>
     View Book <hr> <br>
